233fbe50798d7e79a1e26f80b1190401d270e3d5
gaurikhanna1711 <68925189+gaurikhanna1711@users.noreply.github.com>
2021-01-11 04:04:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="login-register-wrapper">
+      <div classNma='nav-buttons'>Buttons go here </div>
+      <div classNma='form-groups'>Forms go here </div>
+      <div classNma='forgot-panel'>Forgot panel go here </div>
+        
+        
+    </div>
+  );
+}
+
+export default App;
